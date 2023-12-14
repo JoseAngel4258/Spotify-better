fc5e644d95155b34efc78698419abb6cd7fd25b1
unknown <josebravojs1810@gmail.com>
2023-12-14 06:14:29 -0800

    first commit

---
+++ client/src/Login.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const AUTH_URL =
+  'https://accounts.spotify.com/authorize?client_id=5c2b18f5ed6f457d94d0697373b78948&response_type=code&redirect_uri=http://localhost:5173&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%user-read-playback-state%20user-modify-playback-state';
+
+const Login = () => {
+  return (
+    <Container
+      className='d-flex justify-content-center align-items-center'
+      style={{ minHeight: '100vh' }}
+    >
+      <a
+        className='btn btn-success btn-lg'
+        href={AUTH_URL}
+      >
+        Login With Spotify
+      </a>
+    </Container>
+  );
+};
+
+export default Login;
